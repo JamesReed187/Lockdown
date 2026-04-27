eef5044c4c6814e850376c085e745cdd6a3aa318
James Reed <0852520@schoolsnet.act.edu.au>
2026-04-27 21:00:33 +1000

    Massive changes to the textbox
    Nearly finished.

--- scenes/chatbox.gd
+++ scenes/chatbox.gd
@@ -1,14 +1,33 @@
 extends Node2D
 
-@onready var Message = $Message
-@onready var Send = $Send
-@onready var Messages = $Messages
-@onready var chatbox =$Chatbox
+@onready var Message = $Message          
+@onready var Send = $Send                
+@onready var Messages = $Messages        
+@onready var Chatbox = self       
+
+
+func _ready():
+	#Chatbox.hide() Doesnt work right now. Working on a fix. 
+	Send.pressed.connect(_on_send_pressed)
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("chatbox"):
+		Chatbox.visible = !Chatbox.visible   # Toggle visibility
 
 
 func _on_send_pressed() -> void:
-	pass
+	var text = Message.text.strip_edges()
 
-func _ready():
-	if Input.is_action_just_pressed("chatbox"):
-		chatbox.hide()
+	if text == "":
+		return  # Don't send empty messages
+
+	# Create a new label for the message
+	var new_message = Label.new()
+	new_message.text = text
+
+	# Add it to the message box
+	Messages.add_child(new_message)
+
+	
+	Message.text = ""
